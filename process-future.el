374ca20c9ffae953bc128345c81a29d7ed7999c4
Alexander M <alexanderm@web.de>
2017-05-23 22:07:02 +0200

    Use default separator.

--- process-future.el
+++ process-future.el
@@ -4,6 +4,7 @@
 
 ;; Author: Alexander Miller <alexanderm@web.de>
 ;; Homepage: https://github.com/Alexander-Miller/treemacs
+;; Package-Requires: ((emacs "25"))
 ;; Version: 1.0
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -23,12 +24,12 @@
 
 ;;; Code:
 
-(require 'cl-macs)
+(require 'cl-lib)
 
-(cl-defstruct pf::future
+(cl-defstruct pf-future
   process result)
 
-(defun pf::new (&rest args)
+(defun pf-new (&rest args)
   "Create a new process future with ARGS.
 This will return a struct (as created by `cl-defstruct') with 2 fields:
 'process' which is the process object that will be started and 'result', where
@@ -36,20 +37,20 @@ the process will be writing its output.
 
 Note that ARGS must be a *list* of strings as demanded by `make-process'.
 In other words
-This is wrong: (pf::new \"git status\")
-This is right: (pf::new \"git\" \"status\")"
-  (let* ((future  (make-pf::future))
+This is wrong: (pf-new \"git status\")
+This is right: (pf-new \"git\" \"status\")"
+  (let* ((future  (make-pf-future))
          (process (make-process
                    :name "Process Future"
                    :connection-type 'pipe
                    :command args
                    :filter #'(lambda (_ msg)
-                               (let ((result (pf::result-of future)))
-                                 (setf (pf::future-result future) (concat result msg)))))))
-    (setf (pf::future-process future) process)
+                               (let ((result (pf-result-of future)))
+                                 (setf (pf-future-result future) (concat result msg)))))))
+    (setf (pf-future-process future) process)
     future))
 
-(cl-defun pf::await (future &key (timeout 1) (just-this-one t))
+(cl-defun pf-await (future &key (timeout 1) (just-this-one t))
   "Block until FUTURE has produced output and return it.
 The output will also be added to FUTURE's 'result' field.
 
@@ -60,29 +61,29 @@ specify fractional number of seconds. In case of timeout nil will be returned.
 
 JUST-THIS-ONE: When t only read from the process of FUTURE and no other. For
 details see documentation of `accept-process-output'."
-  (when (pf::is-alive? future)
+  (when (pf-is-alive? future)
     (accept-process-output
-     (pf::process-of future) timeout nil just-this-one))
-  (pf::result-of future))
+     (pf-process-of future) timeout nil just-this-one))
+  (pf-result-of future))
 
-(defun pf::await-to-finish (future)
+(defun pf-await-to-finish (future)
   "Keep reading the output of FUTURE until it is done.
-Same as `pf::await', but will keep reading (and blocking) so long as the
+Same as `pf-await', but will keep reading (and blocking) so long as the
 process associated with FUTURE is alive.
 If the process never quits this method will block forever. Use with caution!"
-  (let ((process (pf::process-of future)))
+  (let ((process (pf-process-of future)))
     (while (process-live-p process)
       (accept-process-output process nil nil t)))
-  (pf::result-of future))
+  (pf-result-of future))
 
-(defun pf::is-alive? (future)
+(defun pf-is-alive? (future)
   "Return whether the process associated with FUTURE is alive."
-  (process-live-p (pf::process-of future)))
+  (process-live-p (pf-process-of future)))
 
-(defalias 'pf::process-of #'pf::future-process
+(defalias 'pf-process-of #'pf-future-process
   "Access a process future's process slot.")
 
-(defalias 'pf::result-of #'pf::future-result
+(defalias 'pf-result-of #'pf-future-result
   "Access a process future's result slot.")
 
 (provide 'process-future)
